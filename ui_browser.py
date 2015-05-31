410e3ca342cf69934e2d76a626927b0986a813d5
Roman Sirokov <roman@flowrl.com>
2015-05-31 23:20:18 +0300

    Added UI browser

---
+++ ui_browser.py
@@ -0,0 +1,15 @@
+import webview
+import threading
+import guiserver
+
+
+def start_ui(url):
+    webview.create_window("Traktor Librarian", url)
+
+
+if __name__ == '__main__':
+    server = threading.Thread(target=guiserver.start_webserver, args=(41345,))
+    server.daemon = False
+    server.start()
+
+    start_ui("http://localhost:{}/".format(guiserver.http_port))
